b140afa554345bdbe49ed98fe0741f042ea7e62e
Ankita Khurana <ankita0096@gmail.com>
2017-10-16 16:39:53 +0530

    server.js

--- back_end/server.js
+++ back_end/server.js
@@ -1,5 +1,10 @@
 const express = require('express');
 
-
+const bodyParser = require('body-parser');
 const app = express();
 
+const passport = require('./auth/passport');    //authentication, user
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
