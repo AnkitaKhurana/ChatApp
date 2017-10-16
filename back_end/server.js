d9a9b6841cf231a63da8f9ed342d7cace97f0139
Ankita Khurana <ankita0096@gmail.com>
2017-10-16 16:45:06 +0530

    server.js

--- back_end/server.js
+++ back_end/server.js
@@ -1,3 +1,6 @@
+// const cors = require('cors');
+// app.use(cors());
+
 const express = require('express');
 
 const bodyParser = require('body-parser');
@@ -8,3 +11,17 @@ const passport = require('./auth/passport');    //authentication, user
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+app.use('/api', require('./routes/api'));
+app.use('/', require('./routes/index'));
+app.use('/', express.static(__dirname + "/public_static"));
+
+
+app.listen(3456, function () {
+    console.log("Server started on http://localhost:3456");
+});
