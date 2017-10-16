4faa34ef9425cebf369f10d0d57d00ee006162a8
Ankita Khurana <ankita0096@gmail.com>
2017-10-16 17:58:27 +0530

    changes

--- back_end/server.js
+++ back_end/server.js
@@ -3,23 +3,27 @@
 
 const express = require('express');
 
+const path = require('path');
 const bodyParser = require('body-parser');
 const app = express();
+const expressSession = require('express-session');
 
-const passport = require('./auth/passport');    //authentication, user
+
+// const passport = require('./auth/passport');    //authentication, user
 
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 
-app.use(passport.initialize());
-app.use(passport.session());
+// app.use(passport.initialize());
 
-
+// app.use(passport.session());
+console.log(require('./routes/api'));
 app.use('/api', require('./routes/api'));
+
 app.use('/', require('./routes/index'));
-app.use('/', express.static(__dirname + "/public_static"));
+// app.use('/', express.static(__dirname + "/public_static"));
 
 
 app.listen(3456, function () {
