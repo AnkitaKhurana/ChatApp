80acf06c504789108e8ce631bc584368d8edc3d4
Ankita Khurana <ankita0096@gmail.com>
2017-10-17 23:11:12 +0530

    changes

--- back_end/routes/index.js
+++ back_end/routes/index.js
@@ -3,13 +3,33 @@ const route = require('express').Router();
 const User = require('../db/models').User;
 
 route.get('/',(req,res)=>{
-    res.render('index', { title: 'Elista'});
+    res.render('index', { title: 'CHATAPP'});
 
     // res.send(req.user);
 });
 
 
 
+route.post('/login',passport.authenticate('local',{
+    successRedirect:'/api/temp',
+    failureRedirect:'/'
+}));
+
+
+
+route.get('/logout', (req, res) => {
+    req.user = null;
+    req.logout();
+    req.session.destroy(function () {
+        res.render('index', { title: 'CHATAPP'});
+        // res.redirect('/login.html')
+    })
+});
+
+
+route.get('/profile', eli('/'), (req, res) => {
+    res.send(req.user);
+});
 
 
 module.exports = route;
